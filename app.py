7d675bf09cb0020392e9d2913517219f80de4d83
Ahmed8047762 <an8047762@gmail.com>
2023-10-14 00:46:27 +0500

    functionality till signin added

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = '1893'  # Replace with a secret key for your app
 
+def create_connection():
+    return sqlite3.connect('herdhelp.db')
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -18,10 +21,8 @@ def signup():
             con = conn.cursor()
             con.execute("INSERT INTO user (name, password) VALUES (?, ?)", (username, password))
             
-            con.execute("SELECT * FROM user")
-            result = con.fetchall()
-            print(result)
-            return redirect(url_for('signup'))
+        # Redirect to the signin page after successful signup
+        return redirect(url_for('signin'))
     
     return render_template('signup.html')
 
@@ -32,6 +33,34 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/how_to_use')
+def how_to_use():
+    return render_template('how_to_use.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        conn = create_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM user WHERE name=? AND password=?', (username, password))
+        user = cursor.fetchone()
+        
+        conn.close()
+
+        if user:
+            # Authentication successful
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            # Authentication failed
+            flash('Invalid username or password. Please try again.', 'error')
+            return redirect(url_for('signin'))
+    
+    return render_template('signin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
