8288f0559634e9bee3cbdb7e4c00e1af76cd0035
Ahmed8047762 <an8047762@gmail.com>
2023-10-15 10:30:07 +0500

    made chat interface

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = '1893'  # Replace with a secret key for your app
+app.config['SECRET_KEY'] = '1893'  # Replace with a secret key for your app
 
+# This function creates a connection to a database. The database is stored in a file named "herdhelp.db".
 def create_connection():
     return sqlite3.connect('herdhelp.db')
 
@@ -48,19 +49,51 @@ def signin():
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM user WHERE name=? AND password=?', (username, password))
         user = cursor.fetchone()
-        
-        conn.close()
 
         if user:
             # Authentication successful
-            flash('Login successful!', 'success')
-            return redirect(url_for('index'))
+            user_id = user[0]  # Get the user_id from the database result
+            session['user_id'] = user_id  # Store the user_id in the session
+            print("User id in signin: ", session['user_id'])
+            conn.close()
+            #flash('Login successful!', 'success')
+            return redirect(url_for('chat'))
         else:
             # Authentication failed
             flash('Invalid username or password. Please try again.', 'error')
             return redirect(url_for('signin'))
     
     return render_template('signin.html')
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        
+        # Here, you should use your NLP model to generate a response based on the prompt.
+        # For now, I'll provide a hardcoded response.
+        response = "This is a hardcoded response. Your NLP model will generate the actual response."
+        
+        # Save the prompt and response to the database
+        conn = create_connection()
+        cursor = conn.cursor()
+        user_id = session['user_id']
+        print("User id in chat: ", user_id)
+        cursor.execute("INSERT INTO prompts (user_id, prompt) VALUES (?, ?)", (user_id, prompt))
+        prompt_id = cursor.lastrowid
+        cursor.execute("INSERT INTO responses (prompt_id, response) VALUES (?, ?)", (prompt_id, response))
+        conn.commit()
+        conn.close()
+    
+    # Fetch chat history from the database
+    conn = create_connection()
+    cursor = conn.cursor()
+    user_id = session['user_id']  # Retrieve user ID from the session
+    cursor.execute("SELECT prompts.prompt, responses.response FROM prompts JOIN responses ON prompts.id = responses.prompt_id WHERE prompts.user_id = ? ORDER BY prompts.timestamp ASC", (user_id,))
+    chat_history = cursor.fetchall()
+    conn.close()
+    
+    return render_template('chat.html', chat_history=chat_history)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/HerdHelpDB.py
+++ models/HerdHelpDB.py
@@ -5,21 +5,42 @@ c = conn.cursor()
 
 #creating admin table
 
-c.execute('''CREATE TABLE IF NOT EXISTS user
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL,
-              password TEXT NOT NULL)''')
+#c.execute('''CREATE TABLE IF NOT EXISTS user
+#             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+#              name TEXT NOT NULL,
+#              password TEXT NOT NULL)''')
 
 # Check if the table exists
 c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='user'")
 table_exists = c.fetchone()
 
-if table_exists:
-    print("user table created successfully")
-else:
-    print("failed to create user table")    
+#if table_exists:
+#    print("user table created successfully")
+#else:
+#    print("failed to create user table")    
 #c.execute("INSERT INTO admin (name, password) VALUES (?, ?)", ('ahmed', '0000'))
 
-c.execute("SELECT * FROM user")
+#c.execute("SELECT * FROM user")
+#rows = c.fetchall()
+#print(rows)
+
+# Creating a table to store chat prompts
+c.execute('''CREATE TABLE IF NOT EXISTS prompts
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER,
+              prompt TEXT NOT NULL,
+              timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+
+# Creating a table to store chat responses
+c.execute('''CREATE TABLE IF NOT EXISTS responses
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              prompt_id INTEGER,
+              response TEXT NOT NULL,
+              timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+
+c.execute("SELECT prompts.prompt, responses.response FROM prompts JOIN responses ON prompts.id = responses.prompt_id WHERE prompts.user_id = ? ORDER BY prompts.timestamp DESC", (2,))
 rows = c.fetchall()
 print(rows)
+
+conn.commit()
+conn.close()
